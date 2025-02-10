ecb947a1852a8e5e2ec04f863f5b6d8bc0cb45c4
Glebwen <gleb_shtyrev@mail.ru>
2025-02-10 22:47:55 +0800

    StringCommit
    строковая задача и тесты к ней

---
+++ String-program/String-program/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_program
+{
+    public class Logic
+    {
+        public static bool CheckLine(string line)
+        {
+            for (int i=0; i<line.Length; i++)
+            {
+                if (i%2==0)
+                {
+                    if (!int.TryParse(line[i].ToString(), out int res))
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if(line[i]!='+' && line[i]!='-')
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static string CountLine(string line)
+        {
+            string outMessage = "";
+            if (CheckLine(line))
+            {
+                int result = int.Parse(line[0].ToString());
+                for(int i = 1; i < line.Length; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        if (line[i-1] == '+')
+                        {
+                            result += int.Parse(line[i].ToString());
+                        }
+                        if(line[i - 1] == '-')
+                        {
+                            result -= int.Parse(line[i].ToString());
+                        }
+                    }
+                }
+                outMessage = result.ToString();
+            }
+            else
+            {
+                outMessage = "некорректный ввод";
+            }
+            return outMessage;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите строку цифр, разделенных зхнаками + или -");
+            string line = Console.ReadLine();
+
+
+
+            Console.WriteLine(Logic.CountLine(line));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ String-program/String-programTests/LogicTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using String_program;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_program.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void IncorrectInputTest()
+        {
+            string line = "incorrect";
+
+            Assert.AreEqual(Logic.CountLine(line), "некорректный ввод");
+        }
+
+        [TestMethod()]
+        public void NotCompletelyCorrectInputTest()
+        {
+            string line = "-5+5-5";
+
+            Assert.AreEqual(Logic.CountLine(line), "некорректный ввод");
+        }
+
+        [TestMethod()]
+        public void CorrectInputTest()
+        {
+            string line = "5+5-5+6";
+
+            Assert.AreEqual(Logic.CountLine(line), "11");
+        }
+    }
+}
