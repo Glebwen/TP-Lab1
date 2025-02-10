f88df37ef3f895b0e146cd7a81f75cd61ed7346e
Glebwen <gleb_shtyrev@mail.ru>
2025-02-10 20:58:06 +0800

    IfProgCommit
    первая программа и автотесты к ней

---
+++ if-program/if-program/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_program
+{
+    public class Logic
+    {
+        public static string Convert(string n)
+        {
+            string outMessage;
+            if (!int.TryParse(n, out int x))
+            {
+                outMessage = "некорректный ввод";
+            }
+            else
+            {
+                if (x < 1 || x > 9999)
+                {
+                    outMessage = "число за пределом ОДЗ";
+                }
+                else
+                {
+                    if (x % 100 == 0)
+                    {
+                        outMessage = (x / 100) + " рублей ровно";
+                    }
+                    else
+                    {
+                        outMessage = x / 100 + " рублей " + x % 100 + " копеек";
+                    }
+                }
+
+            }
+            return outMessage;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число 1<=n<=9999");
+            string n = Console.ReadLine();
+
+
+            Console.WriteLine(Logic.Convert(n));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ if-program/if-programTests/LogicTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using if_program;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_program.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void IncorrectInputTest()
+        {
+            string n = "AAAAAAAAA";
+
+            Assert.AreEqual(Logic.Convert(n), "некорректный ввод");
+        }
+
+
+        [TestMethod()]
+        public void OutOfBoundsTest()
+        {
+            string n = "0";
+
+            Assert.AreEqual(Logic.Convert(n), "число за пределом ОДЗ");
+        }
+
+        [TestMethod()]
+        public void IntRubTest()
+        {
+            string n = "5500";
+
+            Assert.AreEqual(Logic.Convert(n), "55 рублей ровно");
+        }
+
+        [TestMethod()]
+        public void NotIntRubTest()
+        {
+            string n = "5545";
+
+            Assert.AreEqual(Logic.Convert(n), "55 рублей 45 копеек");
+        }
+    }
+}
